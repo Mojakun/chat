783c83adf3c44d7991c58bd7bfa910fc3c482bed
Mojakun <kazuya.6289@gmail.com>
2021-10-23 17:51:49 +0900

    userAvatarにクラスから取得したカラーを適応

--- chat_board/src/components/atoms/UserAvatar.jsx
+++ chat_board/src/components/atoms/UserAvatar.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import Avatar from '@mui/material/Avatar';
+import ThemeColorModel from '../../models/ThemeColorModel';
 
-const UserAvater = () => {
-    
-    return(
-        <SAvatar src="https://picsum.photos/200/300" alt="UserAvatar" sx={{ border:3,borderColor:'primary.main'}}></SAvatar>
-    )
+const UserAvater = (props) => {
+  const color = ThemeColorModel.getThemeColor(props.themeColor);
+
+  return (
+    <SAvatar src="https://picsum.photos/200/300" alt="UserAvatar" sx={{ border: 3, borderColor: color }}></SAvatar>
+  );
 };
 
-const SAvatar = styled(Avatar)`
+const SAvatar = styled(Avatar)``;
 
-
-`;
-
-export default UserAvater
+export default UserAvater;
