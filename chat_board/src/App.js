d2b4491b85d5809e2b3e29fdf9d1280aeab118f7
Mojakun <kazuya.6289@gmail.com>
2021-10-23 19:35:30 +0900

    チャットカードコンポーネント作成

--- chat_board/src/App.js
+++ chat_board/src/App.js
@@ -4,13 +4,8 @@ import './App.css';
 import MainLayout from './components/templates/layout/MainLayout';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
-// import ListSubheader from '@mui/material/ListSubheader';
-// import Avatar from '@mui/material/Avatar';
-import UserAvater from './components/atoms/UserAvatar';
 import axios from 'axios';
+import ChatCard from './components/organisms/chat/ChatCard';
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -27,29 +22,7 @@ const App = () => {
       <List sx={{ mb: 2 }}>
         {data.map((d) => (
           <React.Fragment key={d.id}>
-            <ListItem button>
-              <ListItemAvatar>
-                <UserAvater themeColor={d.theme_color} />
-              </ListItemAvatar>
-              <ListItemText
-                primary={
-                  <React.Fragment>
-                    <Typography sx={{ display: 'inline' }} component="span" variant="body2" color="text.primary">
-                    {d.title}
-                    </Typography>
-                    /{d.content}
-                  </React.Fragment>
-                }
-                secondary={
-                  <React.Fragment>
-                    <Typography sx={{ display: 'inline' }} component="span" variant="body2" color="text.primary">
-                    {d.name}
-                    </Typography>
-                    /{d.updated_at || d.created_at}
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
+            <ChatCard chatData={d}/>
           </React.Fragment>
         ))}
       </List>
