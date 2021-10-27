c8a76a0f3b7190c5ae34d92ca75fe11b4a2ea28e
Mojakun <kazuya.6289@gmail.com>
2021-10-27 09:21:29 +0900

    チップコンポーネント作成

--- chat_board/src/components/molecules/CategoryChips.jsx
+++ chat_board/src/components/molecules/CategoryChips.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Chip from '../atoms/Chips';
 
 const CategoryChips = ({ label }) => {
-  return <Chip label={label} color="secondary" variant="outlined" />;
+  return <Chip label={label} color="secondary" variant="outlined" size="small"/>;
 };
 
 export default CategoryChips;
